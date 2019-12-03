9cebfdb5959d5e08fbda3db1a195a87460b686b6
Unknown <unknown@example.com>
2019-12-03 17:11:56 +0100

    Front
    Maj du style sur les pages Home et Création de recette

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  loading: any;
+
+  constructor(private loadingController: LoadingController) { }
+
+  async ionViewWillEnter() {
+    this.loading = await this.loadingController.create({
+      duration: 1000,
+      message: ''
+    });
+
+    this.loading.present();
+
+  }
 
   ngOnInit() {
+    console.log("ok");
   }
 
 }
