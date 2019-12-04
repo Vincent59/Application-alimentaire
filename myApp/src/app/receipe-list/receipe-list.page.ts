c54448955478b8a89d51ad199aecf2af93b2d068
Unknown <unknown@example.com>
2019-12-04 16:48:52 +0100

    Front
    Maj des pages de création et de choix de recette.

--- myApp/src/app/receipe-list/receipe-list.page.ts
+++ myApp/src/app/receipe-list/receipe-list.page.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceipeListPage implements OnInit {
 
+  receipe = [
+    {
+      name: 'Burger Poulet Citron',
+      personne: 2,
+      ingredients: 5,
+      image: 'assets/recette_1.jpg'
+    },
+    {
+      name: 'Salade Asiatique épicée au poulet',
+      personne: 3,
+      ingredients: 6,
+      image: 'assets/recette_2.jpg'
+    },
+    {
+      name: 'Croque madame',
+      personne: 2,
+      ingredients: 5,
+      image: 'assets/recette_3.jpg'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
